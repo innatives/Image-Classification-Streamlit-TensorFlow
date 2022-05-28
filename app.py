03a3dbbe6d6b867334ef32b5bbb5fb897fcadc10
innatives <51743162+innatives@users.noreply.github.com>
2022-05-28 13:59:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,28 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
+class_names = ["Bakteria", "Mozaika", "Pleśń", "Zaraza ziemniaczana"]
 
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
+
+tflite_interpreter = tf.lite.Interpreter(model_path='saved_model.tflite')
+tflite_interpreter.allocate_tensors()
+
+def set_input_tensor(interpreter, image):
+    """Sets the input tensor."""
+    tensor_index = interpreter.get_input_details()[0]['index']
+    input_tensor = interpreter.tensor(tensor_index)()[0]
+    input_tensor[:, :] = image
+
+def get_predictions(input_image):
+    output_details = tflite_interpreter.get_output_details()
+    set_input_tensor(tflite_interpreter, input_image)
+    tflite_interpreter.invoke()
+    tflite_model_prediction = tflite_interpreter.get_tensor(output_details[0]["index"])
+    tflite_model_prediction = tflite_model_prediction.squeeze().argmax(axis = 0)
+    pred_class = class_names[tflite_model_prediction]
+    return pred_class
 
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -12,44 +31,17 @@ def load_model():
 	return model
 
 
-def predict_class(image, model):
+st.title('Choroby pomidorów')
 
-	image = tf.cast(image, tf.float32)
-	image = tf.image.resize(image, [180, 180])
+## Input Fields
+uploaded_file = st.file_uploader("Upload a Image", type=["jpg","png", 'jpeg'])
 
-	image = np.expand_dims(image, axis = 0)
+if uploaded_file is not None:		
+    img = Image.open(uploaded_file)
+    st.image(img, caption="Input Image", width = 400)
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
 
-	prediction = model.predict(image)
-
-	return prediction
-
-
-model = load_model()
-st.title('Flower Classifier')
-
-file = st.file_uploader("Upload an image of a flower", type=["jpg", "png"])
-
-
-if file is None:
-	st.text('Waiting for upload....')
-
-else:
-	slot = st.empty()
-	slot.text('Running inference....')
-
-	test_image = Image.open(file)
-
-	st.image(test_image, caption="Input Image", width = 400)
-
-	pred = predict_class(np.asarray(test_image), model)
-
-	class_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
-
-	result = class_names[np.argmax(pred)]
-
-	output = 'The image is a ' + result
-
-	slot.text('Done')
-
-	st.success(output)
-
+if st.button("Sprawdź pomidora"):
+    suggestion = get_predictions(input_image =img_array)
+    st.success(suggestion)
